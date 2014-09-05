6f3a529f27b7747566d4bf40d39cfb0fe6012c6b
ViBorges <vbda123@gmail.com>
2014-09-05 17:10:24 -0300

    Test

---
+++ MinhasFinancas/src/com/dsw/minhasfinancas/Login.java
@@ -0,0 +1,20 @@
+package com.dsw.minhasfinancas;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class Login extends FragmentActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+		
+		//chamando objetos
+		
+		
+	}
+
+}
