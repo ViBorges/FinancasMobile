f2cebc8bf9acb0a6fa97d193f11d8d6044f3c3a4
priscillagss <priscilla_hasya@windowslive.com>
2014-11-23 15:28:52 -0200

    validation user fields

--- FinancasMobile/src/com/dsw/financasmobile/model/User.java
+++ FinancasMobile/src/com/dsw/financasmobile/model/User.java
@@ -1,6 +1,7 @@
 package com.dsw.financasmobile.model;
 
-public class User {
+
+public class User extends Exception{
 
 	private String name;
 	private String password;
@@ -10,7 +11,7 @@ public class User {
 
 	}
 	
-	public User(String name, String password, String confirmPassword) {
+	public User(String name, String password, String confirmPassword)  throws IllegalArgumentException {
 		this.name = name;
 		this.password = password;
 		this.confirmPassword = confirmPassword;
@@ -20,26 +21,70 @@ public class User {
 		return confirmPassword;
 	}
 
-	public void setConfirmPassword(String confirmPassword) {
-		this.confirmPassword = confirmPassword;
+	public void setConfirmPassword(String confirmPassword) throws IllegalArgumentException {
+	    
+        if (confirmPassword != null && !confirmPassword.equals("")) {
+        	this.confirmPassword = confirmPassword;
+            
+        } else {
+            throw new IllegalArgumentException("Por favor, confirme sua senha.");
+            
+        }
+    
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+	public void setName(String name) throws IllegalArgumentException {
+	    
+        if (name != null && !name.equals("")) {
+        	this.name = name;
+            
+        } else {
+            throw new IllegalArgumentException("Pessoa não pode ter nome vazio ou nulo.");
+            
+        }
+    
+    }
 
 	public String getPassword() {
 		return password;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setPassword(String password)throws IllegalArgumentException {
+	    
+        if (password != null && !password.equals("")) {
+        	this.password = password;
+            
+        } else {
+            throw new IllegalArgumentException("Pessoa não pode ter senha vazia ou nula.");
+            
+        }
+    
 	}
 
+	public boolean validaNome(String name){
+		
+		if(name.length() > 30){
+			return false;
+			
+		}
+		return true;
+		
+	}
+	
+	public boolean validaSenha(String password){
+		
+		if(password.length() > 6){
+			return false;
+			
+		}
+		return true;
+		
+	}
+	
 	@Override
 	public String toString() {
 		return "User [name=" + name + ", password=" + password
